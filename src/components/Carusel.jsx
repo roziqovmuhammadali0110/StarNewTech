76acc030c03e401248bf8035ace77e6e3dc8decf
RoziqovM <mroziqov494@gmail.com>
2024-09-09 14:11:37 +0500

    commit

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -15,10 +15,12 @@ import "swiper/css/scrollbar";
 const Carusel = () => {
   const { t } = useTranslation();
 
+  const projects = t("carousel.projects", { returnObjects: true });
+
   return (
     <div className="container mx-auto" id="portfolio">
       <div className="w-full flex items-center justify-center text-[40px] pt-10 font-medium">
-        <h1>{t("carusel.title")}</h1>
+        <h1>{t("carousel.title")}</h1>
       </div>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
@@ -36,112 +38,20 @@ const Carusel = () => {
         className="min-h-[500px] flex items-center justify-center"
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132696302145_75.png"
-            alt={t("projects.bisyor")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">{t("projects.bisyor")}</h1>
-            <p className="text-[12px] font-medium">{t("projects.bisyor")}</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132696453354_75.png"
-            alt={t("projects.agrobuy")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">{t("projects.agrobuy")}</h1>
-            <p className="text-[12px] font-medium">{t("projects.agrobuy")}</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132696624512_75.png"
-            alt={t("projects.xalq_talim")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">
-              {t("projects.xalq_talim")}
-            </h1>
-            <p className="text-[12px] font-medium">
-              {t("projects.xalq_talim")}
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132696805116_75.png"
-            alt={t("projects.dana")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">{t("projects.dana")}</h1>
-            <p className="text-[12px] font-medium">{t("projects.dana")}</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132697076925_75.png"
-            alt={t("projects.karyera_xizmati")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">
-              {t("projects.karyera_xizmati")}
-            </h1>
-            <p className="text-[12px] font-medium">
-              {t("projects.karyera_xizmati")}
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132697237531_75.png"
-            alt={t("projects.profi_university")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">
-              {t("projects.profi_university")}
-            </h1>
-            <p className="text-[12px] font-medium">
-              {t("projects.profi_university")}
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/1713269751821_75.png"
-            alt={t("projects.ichki_hujjat")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">
-              {t("projects.ichki_hujjat")}
-            </h1>
-            <p className="text-[12px] font-medium">
-              {t("projects.ichki_hujjat")}
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/1713269765955_75.png"
-            alt={t("projects.ijod_uz")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">{t("projects.ijod_uz")}</h1>
-            <p className="text-[12px] font-medium">{t("projects.ijod_uz")}</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flex flex-col items-center justify-center">
-          <img
-            src="https://progress-solution.uz/storage/projects/17132697791096_75.png"
-            alt={t("projects.kzo_fvv")}
-          />
-          <div className="flex items-end gap-2">
-            <h1 className="text-[18px] font-medium">{t("projects.kzo_fvv")}</h1>
-            <p className="text-[12px] font-medium">{t("projects.kzo_fvv")}</p>
-          </div>
-        </SwiperSlide>
+        {projects.map((project, index) => (
+          <SwiperSlide
+            key={index}
+            className="flex flex-col items-center justify-center">
+            <img
+              src={`https://progress-solution.uz/storage/projects/${project.image}`}
+              alt={project.name}
+            />
+            <div className="flex items-end gap-2">
+              <h1 className="text-[18px] font-medium">{project.name}</h1>
+              <p className="text-[12px] font-medium">{project.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
